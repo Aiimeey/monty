8f73790c84142f7008e3792f8e6e815606cb7d26
imane <imanezenasnii@gmail.com>
2023-09-21 22:29:05 +0100

    Added a function for validating input parameters in the Monty bytecode interpreter

--- check.c
+++ check.c
@@ -1,5 +1,11 @@
 #include "monty.h"
-
+/**
+ * is_numeric - Checks if a string consists of numeric characters
+ * @str: The string to be checked
+ *
+ * Return: 0 if the string contains only numeric characters
+ *         EXIT_FAILURE if it contains non-numeric characters
+ */
 int is_numeric(char *str)
 {
 	int i;
@@ -12,4 +18,36 @@ int is_numeric(char *str)
 		}
 	}
 	return (0);
+}
+
+/**
+ * no_int_error - Prints an error message for push with a non-integer argument
+ * @line_number: The line number in the Monty bytecode file
+ * Return: exit the program.
+ */
+
+int no_int_error(int line_number)
+{
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	exit(EXIT_FAILURE);
+}
+/**
+ * pint_error - Prints an error message for pint with an empty stack
+ * @line_number: The line number in the Monty bytecode file
+ * Return: exit the program
+ */
+int pint_error(int line_number)
+{
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+	exit(EXIT_FAILURE);
+}
+/**
+ * pop_error - Prints an error message for pop with an empty stack
+ * @line_number: The line number in the Monty bytecode file
+ * Return: exit the program
+ */
+int pop_error(int line_number)
+{
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
 }
