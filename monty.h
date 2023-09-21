4af83fb0a3e2873fdaab25b4cec594d914a7514a
imane <imanezenasnii@gmail.com>
2023-09-21 21:06:18 +0100

    Added main header file 'monty.h'

---
+++ monty.h
@@ -0,0 +1,66 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern char *n;
+
+int usage_error(void);
+int f_open_error(char *file);
+int unknown_op_error(char *op_code, int line_number);
+int malloc_error(void);
+int no_int_error(int line_number);
+int pint_error(int line_number);
+int int_error(int line_number);
+int pop_error(int line_number);
+
+int is_numeric(char *str);
+
+typedef void (*f)(stack_t **stack, unsigned int line_number);
+f get_func(char *op_code);
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
+#endif
