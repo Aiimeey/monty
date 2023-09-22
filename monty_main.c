7de665e8549d1bf1920b7ca33bcedd4cbea35756
imane <imanezenasnii@gmail.com>
2023-09-22 09:43:56 +0100

    Apply Betty Style Corrections

--- monty_main.c
+++ monty_main.c
@@ -10,28 +10,36 @@ char *n;
 int main(int argc, char **argv)
 {
 	void (*f)(stack_t **stack, unsigned int line_number);
-	char line[1024], *op_code, char *delimiter = " \t\n";
+	char line[1024], *op_code;
 	FILE *file;
 	int line_number = 0;
 	stack_t *stack = NULL;
+	char *delimiter = " \t\n";
 
 	if (argc != 2)
 		return (usage_error());
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 		return (f_open_error(argv[1]));
+
 	while (fgets(line, sizeof(line), file) != NULL)
 	{
 		line_number++;
 		op_code = strtok(line, delimiter);
+
 		do {
 			if (strcmp(op_code, "\n") == 0 || strcmp(op_code, "\t") == 0)
+			{
 				op_code = strtok(NULL, delimiter);
+			}
 			else
 				break;
 		} while (op_code);
+
 		if (unknown_op_error(op_code, line_number) != 0)
 			exit(EXIT_FAILURE);
+
 		if (strcmp(op_code, "push") == 0)
 		{
 			n = strtok(NULL, delimiter);
@@ -41,9 +49,12 @@ int main(int argc, char **argv)
 				exit(EXIT_FAILURE);
 			}
 		}
+
 		f = get_func(op_code);
 		if (f != NULL)
+		{
 			f(&stack, line_number);
+		}
 	}
 	free(stack);
 	fclose(file);
